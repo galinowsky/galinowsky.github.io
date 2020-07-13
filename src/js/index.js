6e4f15bfc4e61c9e546657b04c821a5a25958116
Jakub Galinski <thougless@gmail.com>
2020-07-13 23:51:49 +0200

    feat: added js, fetch api, local storage for projects and

--- src/js/index.js
+++ src/js/index.js
@@ -1,4 +1,16 @@
 import '../scss/main.scss';
+import plainProject from "../pages/project.html";
 
+fetch("https://api.github.com/users/galinowsky/repos?sort=created")
+  .then((resp) => resp.json())
+  .then((resp) => {
+    console.log(resp);
+    localStorage.setItem("Projects", JSON.stringify(resp));
+  })
+  .catch(console.log((error) => console.log("error")));
 
-console.log('HELLO 🚀')
+let Projects = JSON.parse(localStorage.getItem("Projects"));
+console.log('dupa')
+for(const project of Projects){
+console.log(project)
+}
