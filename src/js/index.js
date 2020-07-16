ceeb3faf4bccf339d241bdd197d795f7f9592208
Jakub Galinski <thougless@gmail.com>
2020-07-16 18:32:30 +0200

    feat: working listing, adding name, desc and source link

--- src/js/index.js
+++ src/js/index.js
@@ -10,20 +10,25 @@ import plainProject from "../pages/project.html";
 //   .catch(console.log((error) => console.log("error")));
 
 let Projects = JSON.parse(localStorage.getItem("Projects"));
-console.log('dupa')
+
 
 const projList = document.querySelector('.github-list')
 for(const project of Projects){
   const {name,html_url, description} = project
-  // console.log(`name is: ${name}`)
-  // console.log(`html url is: ${html_url}`)
-  // console.log(`description is: ${description}`)
   var singleProject = document.createRange().createContextualFragment(plainProject);
   // console.log(singleProject.children[0].lastElementChild)
-
-  // console.log(singleProject);
-  
   projList.appendChild(singleProject)
-  projList.lastElementChild.id = name;
+  let thisProj  = projList.lastElementChild
+  thisProj.id = name;
+
+  let title = thisProj.querySelector('.github__project--content')
+  let desc = thisProj.querySelector('.github__description--content')
+  let demoLink = thisProj.querySelector('.github__demo--content')
+  let sourceLink = thisProj.querySelector('.github__source--content')
+  
+  title.innerHTML = name
+  desc.innerHTML = description
+  sourceLink.setAttribute('href', html_url) 
+
 }
 
