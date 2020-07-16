3567beb36d8d4e691bd63fbc0def167e265fe8bd
Jakub Galinski <thougless@gmail.com>
2020-07-16 18:12:06 +0200

    feat: listing proj elements and assigning names as id

--- src/js/index.js
+++ src/js/index.js
@@ -12,14 +12,18 @@ import plainProject from "../pages/project.html";
 let Projects = JSON.parse(localStorage.getItem("Projects"));
 console.log('dupa')
 
-const projList = document.querySelector('github-list')
+const projList = document.querySelector('.github-list')
 for(const project of Projects){
   const {name,html_url, description} = project
   // console.log(`name is: ${name}`)
   // console.log(`html url is: ${html_url}`)
   // console.log(`description is: ${description}`)
   var singleProject = document.createRange().createContextualFragment(plainProject);
-  console.log(singleProject.children[0].lastElementChild)
+  // console.log(singleProject.children[0].lastElementChild)
 
+  // console.log(singleProject);
+  
+  projList.appendChild(singleProject)
+  projList.lastElementChild.id = name;
 }
 
