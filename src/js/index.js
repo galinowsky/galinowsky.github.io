c8f69492d6c082196624e852a22aa3996091fcc0
Jakub Galinski <thougless@gmail.com>
2020-09-29 00:26:17 +0200

    updated skills list, fixed fetch api problem

--- src/js/index.js
+++ src/js/index.js
@@ -1,35 +1,35 @@
-import '../scss/main.scss';
+import "../scss/main.scss";
 import plainProject from "../pages/project.html";
 
 fetch("https://api.github.com/users/galinowsky/repos?sort=created")
   .then((resp) => resp.json())
   .then((resp) => {
-    console.log(resp);
-    localStorage.setItem("Projects", JSON.stringify(resp));
+    writeProjects(resp);
   })
   .catch(console.log((error) => console.log("error")));
 
-let Projects = JSON.parse(localStorage.getItem("Projects"));
+const writeProjects = (Projects) => {
+  const projList = document.querySelector(".github-list");
+  for (const project of Projects) {
+    const { name, html_url, description, homepage } = project;
+    if (name != "galinowsky.github.io") {
+      var singleProject = document
+        .createRange()
+        .createContextualFragment(plainProject);
+      // console.log(singleProject.children[0].lastElementChild)
+      projList.appendChild(singleProject);
+      let thisProj = projList.lastElementChild;
+      thisProj.id = name;
 
+      let title = thisProj.querySelector(".github__project--content");
+      let desc = thisProj.querySelector(".github__description--content");
+      let demoLink = thisProj.querySelector(".github__demo--content");
+      let sourceLink = thisProj.querySelector(".github__source--content");
 
-const projList = document.querySelector('.github-list')
-for(const project of Projects){
-  const {name,html_url, description, homepage} = project
-  var singleProject = document.createRange().createContextualFragment(plainProject);
-  // console.log(singleProject.children[0].lastElementChild)
-  projList.appendChild(singleProject)
-  let thisProj  = projList.lastElementChild
-  thisProj.id = name;
-
-  let title = thisProj.querySelector('.github__project--content')
-  let desc = thisProj.querySelector('.github__description--content')
-  let demoLink = thisProj.querySelector('.github__demo--content')
-  let sourceLink = thisProj.querySelector('.github__source--content')
-  
-  title.innerHTML = name
-  desc.innerHTML = description
-  sourceLink.setAttribute('href', html_url) 
-  demoLink.setAttribute('href', homepage)
-
-}
-
+      title.innerHTML = name;
+      desc.innerHTML = description;
+      sourceLink.setAttribute("href", html_url);
+      demoLink.setAttribute("href", homepage);
+    }
+  }
+};
